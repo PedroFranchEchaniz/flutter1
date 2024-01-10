ab8ab162c781e706709960b7732de94ccc54cc6d
Pedro Franch Echaniz <pedrofe.av@gmail.com>
2024-01-10 14:06:30 +0100

    ejercicio 1

--- lib/login.dart
+++ lib/login.dart
@@ -11,25 +11,27 @@ class Login extends StatelessWidget {
         color: const Color(0xFFE62F16),
         child: Column(
           children: [
-            Image.asset(''),
-            const Text(''),
-            ElevatedButton(onPressed: () {}, child: const Text('')),
-            const Text(''),
-            ElevatedButton(onPressed: () {}, child: const Text('')),
-            RichText(
-              text: const TextSpan(
-                text: 'Terms of Use ',
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Color.fromARGB(255, 255, 255, 255)),
-                children: <TextSpan>[
-                  TextSpan(
-                      text: 'and',
-                      style: TextStyle(fontWeight: FontWeight.bold)),
-                  TextSpan(
-                      text: ' world!',
-                      style: TextStyle(fontWeight: FontWeight.bold)),
-                ],
+            Expanded(child: Image.asset('')),
+            const Expanded(child: Text('Lorem impsum')),
+            ElevatedButton(onPressed: () {}, child: const Text('Boton')),
+            const Expanded(child: Text('Lorem impsum')),
+            ElevatedButton(onPressed: () {}, child: const Text('Boton')),
+            Expanded(
+              child: RichText(
+                text: const TextSpan(
+                  text: 'Terms of Use ',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 255, 255, 255)),
+                  children: <TextSpan>[
+                    TextSpan(
+                        text: 'and',
+                        style: TextStyle(fontWeight: FontWeight.bold)),
+                    TextSpan(
+                        text: ' world!',
+                        style: TextStyle(fontWeight: FontWeight.bold)),
+                  ],
+                ),
               ),
             ),
           ],
